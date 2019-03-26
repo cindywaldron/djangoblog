2d1834517e32697ebdcbef7ca377e64c2915185f
Cindy Waldron <cindywaldron@gmail.com>
2019-03-25 18:34:13 -0700

    RSS feed

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import list_view, detail_view, add_model
-
+from myblog.feed import LatestEntriesFeed
 
 urlpatterns = [
     path('',list_view,name="blog_index"),
     path('posts/<int:post_id>/',detail_view, name="blog_detail"),
     path('new_post',add_model, name="new_post"),
+    path('latest/feed/', LatestEntriesFeed()),
 ]
